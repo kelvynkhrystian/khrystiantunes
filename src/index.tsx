19065ed7660c35d7c82a310acaf30f8497967655
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-07-14 11:36:23 -0300

    add-global-style

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import GlobalStyle from './styles/globalStyles';
+import { GlobalStyle } from './styles/globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-    {/* <GlobalStyle /> */}
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
