69fb1d6959dc5dd3343a2cbcba142ea37c883c18
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-07-15 10:47:54 -0300

    ajuste-global-styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,20 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin-bottom: ${(props) => props.theme.spacing.sm};
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+  /* reset-css */
+
   * {
     box-sizing: border-box;
-    font-family: 'Open Sans', sans-serif;
+    font-family: -apple-system, 'Open Sans', sans-serif;
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
   }
 
 `;
