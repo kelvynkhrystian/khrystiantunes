dcd8578a590be8afe484b1bcb728a3fa087eed7f
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-07-17 11:37:39 -0300

    header-ok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,47 +8,29 @@ import { HeaderStyles } from '../styles/components/HeaderStyles';
 const Header = () => {
   const { user } = useContext(UserContext);
 
-  // console.log('user', user);
-  // console.log('Dados do usuário:', user.name);
-
   return (
-    <HeaderStyles data-testid="header-component" className={'styles.header'}>
-      <section className={'styles.headerTop'}>
+    <HeaderStyles>
+      <section>
         <img
           src="https://github.com/kelvynkhrystian/khrystiantunes/blob/main/src/images/logoWhite.png?raw=true"
           alt="LogoKhrystianTunes"
         />
-        <div className={'styles.headerUser'}>
+        <div>
           <img
             src="https://github.com/kelvynkhrystian/khrystiantunes/blob/main/src/images/user.png?raw=true"
             alt="LogoUser"
-            className={'styles.imgUser'}
           />
-          <p className={'styles.nameUser'}>
-            Olá, {!user.name ? <Loading /> : user.name}
-          </p>
+          <p>Olá, {!user.name ? <Loading /> : user.name}</p>
         </div>
       </section>
       <nav>
-        <Link
-          to="/search"
-          className={'styles.link'}
-          data-testid="link-to-search"
-        >
+        <Link to="/search">
           <h3>Search</h3>
         </Link>
-        <Link
-          to="/favorites"
-          className={'styles.link'}
-          data-testid="link-to-favorites"
-        >
+        <Link to="/favorites">
           <h3>Favorites</h3>
         </Link>
-        <Link
-          to="/profile"
-          className={'styles.link'}
-          data-testid="link-to-profile"
-        >
+        <Link to="/profile">
           <h3>Profile</h3>
         </Link>
       </nav>
